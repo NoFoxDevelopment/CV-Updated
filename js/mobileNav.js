2f8fece9821fc992ba7af1a29263e7de1bcde1e5
Aaron C <31908496+NoFoxDevelopment@users.noreply.github.com>
2017-10-19 14:34:25 -0400

    Completed Mobile Version V1
    Mobile version v1
    
    TODO:
    -fix JSTennis screen size on mobile

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,3 +1,14 @@
-function navToggle() {
-	$('.navbarMobile').slideToggle();
-}
+var toggled = false;
+
+$(document).ready(function(){
+	$('#mobileNavIcon').click(function(){
+		$(this).toggleClass('open');
+		$('.navbarMobile').slideToggle();
+		if (toggled === false) {
+			toggled = true;
+			$(this).css({marginTop: '+=215px'});
+		} else {
+			toggled = false;
+			$(this).css({marginTop: '-=215px'})};
+	});
+});
