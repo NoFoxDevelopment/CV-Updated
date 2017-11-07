6674a185754c9abd65aa99428cdd98573cf1f212
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2017-11-07 17:58:51 -0500

    Added Footer

---
+++ js/Angular/controllers/constructionApp.js
@@ -0,0 +1,5 @@
+app.controller('constController', ["$scope", function ($scope) {
+        $scope.title = 'Pardon My Mess';
+        $scope.img = '/imgs/foxPC.png';
+        $scope.blurb = 'Whoops! Looks like this page isn\'t quite ready for your eyes yet! I\m working hard to get this ready for you though, so feel free to check back soon!';
+    }]);
