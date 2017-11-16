5a3f3d727dcdb5a7ed9091aa57ca8198fa3ba718
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2017-11-15 19:45:55 -0500

    Added Color Game and minor formatting fixes

--- js/jstennis.js
+++ js/jstennis.js
@@ -22,17 +22,17 @@ if (!isMobile()) {
 
 	var canvas = document.getElementById('gameCanvas');
 	var ctx = canvas.getContext('2d');
-	var x = canvas.width/2;
-	var y = canvas.height/2+30;
+	var x = canvas.width / 2;
+	var y = canvas.height / 2 + 30;
 	var xv = 2;
 	var yv = -2;
 	var ballRadius = 5;
 	var paddleHeight = 10;
 	var paddleWidth = 80;
-	var paddleX = (canvas.width-paddleWidth)/2;
+	var paddleX = (canvas.width - paddleWidth) / 2;
 	var aiPaddleHeight = 10;
 	var aiPaddleWidth = 80;
-	var aiPaddleX = (canvas.width-paddleWidth)/2;
+	var aiPaddleX = (canvas.width - paddleWidth) / 2;
 	var rightPressed = false;
 	var leftPressed = false;
 	var drawing = setInterval(draw, 10);
@@ -41,28 +41,28 @@ if (!isMobile()) {
 	var AISpeed = 2.5;
 
 	//Track Mouse Position relative to canvas for precise paddle control
-	canvas.addEventListener('mousemove', function(e) {
+	canvas.addEventListener('mousemove', function (e) {
 		var relativeX = e.clientX - canvas.offsetLeft;
-		paddleX = relativeX - paddleWidth/2;
+		paddleX = relativeX - paddleWidth / 2;
 	})
 
 	function drawBall() {
 		ctx.beginPath();
-		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+		ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
 		ctx.fillStyle = '#80D414';
 		ctx.fill();
 		ctx.closePath();
 	}
 
-	function drawPaddle(){
+	function drawPaddle() {
 		ctx.beginPath();
-		ctx.rect(paddleX, canvas.height-paddleHeight-5, paddleWidth, paddleHeight);
+		ctx.rect(paddleX, canvas.height - paddleHeight - 5, paddleWidth, paddleHeight);
 		ctx.fillStyle = '#FFFFFF';
 		ctx.fill();
 		ctx.closePath();
 	}
 
-	function drawAIPaddle(){
+	function drawAIPaddle() {
 		ctx.beginPath();
 		ctx.rect(aiPaddleX, 5, aiPaddleWidth, aiPaddleHeight);
 		ctx.fillStyle = '#FFFFFF';
@@ -73,7 +73,7 @@ if (!isMobile()) {
 	function getRandomColor() {
 		var letters = '0123456789ABCDEF';
 		var color = '#';
-		for (var i=0; i<6; i++) {
+		for (var i = 0; i < 6; i++) {
 			color += letters[Math.floor(Math.random() * 16)];
 		}
 
@@ -85,13 +85,13 @@ if (!isMobile()) {
 		//Black background
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.fillStyle = '#000000';
-		ctx.fillRect(0,0,canvas.width,canvas.height);
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 		//Net
 		ctx.beginPath();
 		ctx.setLineDash([5, 5]);
-		ctx.moveTo(0, canvas.height/2);
-		ctx.lineTo(canvas.width, canvas.height/2);
+		ctx.moveTo(0, canvas.height / 2);
+		ctx.lineTo(canvas.width, canvas.height / 2);
 		ctx.lineWidth = 2;
 		ctx.strokeStyle = 'white';
 		ctx.stroke();
@@ -104,21 +104,21 @@ if (!isMobile()) {
 		//Player Score
 		ctx.fillStyle = 'white';
 		ctx.font = '20px Courier';
-		ctx.fillText(playerScore, 25, canvas.height-25);
+		ctx.fillText(playerScore, 25, canvas.height - 25);
 
 
 		drawBall();
 		drawPaddle();
 		drawAIPaddle();
 
-		if(x + xv > canvas.width-ballRadius || x + xv < ballRadius) { //Bounce off walls
+		if (x + xv > canvas.width - ballRadius || x + xv < ballRadius) { //Bounce off walls
 			xv = -xv;
 		}
-		if(y + yv - 9 < ballRadius) {
-			if(x > aiPaddleX && x < aiPaddleX + aiPaddleWidth) {
-				yv = -yv*1.05;
-				let dx = x-(aiPaddleX+aiPaddleWidth/2);
-				xv = dx*0.15; //Simple angle control and bounce-back when hitting the paddle with slightly increased ball speed.
+		if (y + yv - 9 < ballRadius) {
+			if (x > aiPaddleX && x < aiPaddleX + aiPaddleWidth) {
+				yv = -yv * 1.05;
+				let dx = x - (aiPaddleX + aiPaddleWidth / 2);
+				xv = dx * 0.15; //Simple angle control and bounce-back when hitting the paddle with slightly increased ball speed.
 			}
 			else {
 				if (playerScore === 0 || playerScore === 15) {
@@ -134,17 +134,17 @@ if (!isMobile()) {
 				else if (playerScore === 40) {
 					ctx.fillStyle = '#0070E3';
 					ctx.font = "30px Courier";
-					ctx.fillText('YOU WIN!', 160, canvas.height/2);
-					document.getElementById('reloadButton').style.display='block';
+					ctx.fillText('YOU WIN!', 160, canvas.height / 2);
+					document.getElementById('reloadButton').style.display = 'block';
 					endGame();  //AI misses ball, so win if playerscore is 40, then give "Play Again" button to reload page and allow another match.
 				}
 			}
 		}
-		else if(y + yv + 9 > canvas.height-ballRadius) {
-			if(x > paddleX && x < paddleX + paddleWidth) {
-				yv = -yv*1.05;
-				let dx = x-(paddleX+paddleWidth/2);
-				xv = dx*0.15; //Simple angle control and bounce-back when hitting the paddle with slightly increased ball speed.
+		else if (y + yv + 9 > canvas.height - ballRadius) {
+			if (x > paddleX && x < paddleX + paddleWidth) {
+				yv = -yv * 1.05;
+				let dx = x - (paddleX + paddleWidth / 2);
+				xv = dx * 0.15; //Simple angle control and bounce-back when hitting the paddle with slightly increased ball speed.
 			}
 			else {
 				if (AIScore === 0 || AIScore === 15) {
@@ -160,14 +160,14 @@ if (!isMobile()) {
 				else if (AIScore === 40) {
 					ctx.fillStyle = '#0070E3';
 					ctx.font = "30px Courier";
-					ctx.fillText('AI WIN!', 160, canvas.height/2);
-					document.getElementById('reloadButton').style.display='block';
+					ctx.fillText('AI WIN!', 160, canvas.height / 2);
+					document.getElementById('reloadButton').style.display = 'block';
 					endGame(); //Player misses ball, so lose if AIscore is 40, then give "Play Again" button to reload page and allow another match.
 				}
 			}
 		}
 
-		if (aiPaddleX+aiPaddleWidth/2 < x) { //Basic "Shaky Paddle AI" will track ball at predetermined speed. This means players will need to use Angle control to score against AI.
+		if (aiPaddleX + aiPaddleWidth / 2 < x) { //Basic "Shaky Paddle AI" will track ball at predetermined speed. This means players will need to use Angle control to score against AI.
 			aiPaddleX += AISpeed;
 		} else {
 			aiPaddleX -= AISpeed;
@@ -182,9 +182,9 @@ if (!isMobile()) {
 	}
 
 	function reset() { //Reset ball position and velocity.
-		x=canvas.width/2;
-		y=canvas.height/2;
-		xv=2;
-		yv=-2;
+		x = canvas.width / 2;
+		y = canvas.height / 2;
+		xv = 2;
+		yv = -2;
 	}
 }
--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,14 +1,15 @@
 var toggled = false;
 
-$(document).ready(function(){
-	$('#mobileNavIcon').click(function(){
+$(document).ready(function () {
+	$('#mobileNavIcon').click(function () {
 		$(this).toggleClass('open');
 		$('.navbarMobile').slideToggle();
 		if (toggled === false) {
 			toggled = true;
-			$(this).css({marginTop: '+=225px'});
+			$(this).css({ marginTop: '+=275px' });
 		} else {
 			toggled = false;
-			$(this).css({marginTop: '-=225px'})};
+			$(this).css({ marginTop: '-=275px' });
+		}
 	});
 });
